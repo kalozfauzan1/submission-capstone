66bd5667bd54b761d8f6fc27ce43250c03b9c96b
digitalactivation154@gmail.com <digitalactivation154@gmail.com>
2023-01-19 02:24:07 +0700

    Add first workflow

---
+++ submission-capstoneUITests/submission_capstoneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  submission_capstoneUITestsLaunchTests.swift
+//  submission-capstoneUITests
+//
+//  Created by MacbookPro13 on 19/01/23.
+//
+
+import XCTest
+
+final class submission_capstoneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
